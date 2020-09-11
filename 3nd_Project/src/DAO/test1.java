41794903948dadb3f0702cfac61fb3ea208fc052
smhrd <smhrd@DESKTOP-FC6LDH7>
2020-09-11 09:11:13 +0900

    인코딩 수정

--- 3nd_Project/src/DAO/test1.java
+++ 3nd_Project/src/DAO/test1.java
@@ -3,8 +3,12 @@ package DAO;
 public class test1 {
 
 	public static void main(String[] args) {
-		String name = "�̽�ȯ";
+		String name = "이승환";
 
+	} 
+	public void woo() {
+		String a = "박우현" ; 
+		
 	}
 
 }
